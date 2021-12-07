d6d5ceae329ddbe9786c0d2c026da49779212fbe
emmavdbold <emma.vdbold@gmail.com>
2021-12-07 12:31:43 +0100

    Files and results for task 3

--- assignments/04/scripts/bleu.sh
+++ assignments/04/scripts/bleu.sh
@@ -7,19 +7,28 @@ src=fr
 tgt=en
 data=$base/data/$tgt-$src/
 translations=$assign4/translations
+translations_ln=$assign4/translations_ln
+translations_bs=$assign4/translations_bs
+
+# New folders
 bleu_scores=$assign4/bleu_scores
+bleu_scores_ln=$assign4/bleu_scores_ln
+bleu_scores_bs=$assign4/bleu_scores_bs
 
-# Create folder for files with bleu scores
+# Create folders for files with bleu scores
 mkdir -p $bleu_scores
+mkdir -p $bleu_scores_ln
+mkdir -p $bleu_scores_bs
 
-# Get beam size
+# Get beam size and alpha
 k=$1
+a=$2
 
 # change into base directory to ensure paths are valid
 cd $base
 
 # Undo preprocessing
-bash $base/scripts/postprocess.sh $translations/translations_$k.txt $translations/translations_$k.p.txt $tgt
+bash $base/scripts/postprocess.sh $translations_bs/translations_$k.txt $translations_bs/translations_$k.p.txt $tgt
 
 # Calculate BLEU
-cat $translations/translations_$k.p.txt | sacrebleu $data/raw/test.en > $bleu_scores/bleu_$k.txt
+cat $translations_bs/translations_$k.p.txt | sacrebleu $data/raw/test.en > $bleu_scores_bs/bleu_$k.txt
--- assignments/04/scripts/translate.sh
+++ assignments/04/scripts/translate.sh
@@ -6,13 +6,20 @@ base=$scripts/../../..
 src=fr
 tgt=en
 data=$base/data/$tgt-$src/
+
+# New folders
 translations=$assign4/translations
+translations_ln=$assign4/translations_ln
+translations_bs=$assign4/translations_bs
 
-# Create folder for translations
+# Create folders for translations
 mkdir -p $translations
+mkdir -p $translations_ln
+mkdir -p $translations_bs
 
-# Get beam size from command line
+# Get beam size and alpha for length normalisation from command line
 k=$1
+a=$2
 
 # change into base directory to ensure paths are valid
 cd $base
@@ -22,5 +29,6 @@ python translate_beam.py \
     --data $data/prepared \
     --dicts $data/prepared \
     --checkpoint-path $assign4/baseline/checkpoints/checkpoint_best.pt \
-    --output $translations/translations_$k.txt \
-    --beam-size $k
+    --output $translations_bs/translations_$k.txt \
+    --beam-size $k \
+    --alpha $a
